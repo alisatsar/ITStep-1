89eb3de0b87ff70f72a1284e0484daf8b680159b
Alisa Tsarova <alisacookee@gmail.com>
2017-09-28 19:33:53 +0300

    HomeWork
    Only task 1, because task 2 was execute in the lesson

---
+++ 2017.09.19_HomeWork/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*1. В С # индексация начинается с нуля, но в некоторых языках программирования это не так.
+Например, в Turbo Pascal индексация массиве начинается с 1.
+Напишите класс RangeOfArray, который позволяет работать с массивом такого типа,
+в котором индексный диапазон устанавливается пользователем. 
+Например, в диапазоне от 6 до 10, или от –9 до 15.
+Подсказка: В классе можно объявить две переменных, 
+которые будут содержать верхний и нижний индекс допустимого диапазона.*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19_HomeWork_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray array = new RangeOfArray(-14, 5);
+            
+            array[-1] = 5;
+
+            Console.WriteLine(array[-5]);            
+        }
+    }
+}
---
+++ 2017.09.19_HomeWork/task1/UserIndexArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017._09._19_HomeWork_task1
+{
+    public class RangeOfArray
+    {
+        private int beginIndex;
+        private int endIndex;        
+        private int[] array;
+
+        
+        public RangeOfArray(int begin, int end)
+        {
+            beginIndex = begin;
+            endIndex = end > beginIndex ? end : beginIndex + 1;
+
+            array = new int[endIndex - begin];
+        }                 
+
+        public int this[int i]
+        {
+            get
+            {
+                int index;
+                if (i >= beginIndex && endIndex - beginIndex <= array.Length)
+                {
+                    index = Math.Abs(beginIndex) - Math.Abs(i);
+                    return array[index];
+                }
+
+                throw new ArgumentOutOfRangeException();
+            }            
+            set
+            {
+                int index;
+                if (i >= beginIndex && endIndex - beginIndex <= array.Length)
+                {
+                    index = Math.Abs(beginIndex) - Math.Abs(i);
+                    array[index] = value;
+                }
+
+                //throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        
+    }
+}
